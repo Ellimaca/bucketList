a8e279ec7559ffbfca36022b8182bdb727d7495d
Ellimaca <camille.a@live.fr>
2021-03-25 16:23:09 +0100

    arrachage de cheveux sur tp surprise !

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -12,19 +12,34 @@ class MainController extends AbstractController
 {
 
     /**
-     * @Route("/home")
+     * @Route("/home", name="main_home")
      */
     public function home() {
-
-        return $this->render('main/home.html.twig');
+        return $this->render('main/home.html.twig', );
     }
 
     /**
-     * @Route("/test")
+     * @Route("/test", name="main_test")
      */
     public function test() {
 
         return $this->render('main/test.html.twig');
+    }
+
+    /**
+     * @Route("/contact", name="main_contact")
+     */
+    public function contact() {
+
+        return $this->render('main/contact.html.twig');
+    }
+
+    /**
+     * @Route("/reservation", name="main_reservation")
+     */
+    public function reservation() {
+
+        return $this->render('main/reservation.html.twig');
     }
 
 }
